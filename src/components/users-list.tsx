63d8954db334030ebfc0dc1ce1be085fa1380f8a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-17 14:27:47 -0300

    feat: add a component to list users in a table

---
+++ src/components/users-list.tsx
@@ -0,0 +1,32 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/table';
+
+export function UsersList() {
+	const users = [{
+		id: 1,
+		name: 'Victor Badaró',
+		email: 'victor.badaro@fatec.sp.gov.br'
+	}];
+
+	return (
+		<div className="border border-zinc-700 rounded-lg overflow-hidden">
+			<Table className="text-zinc-400">
+				<TableHeader className="bg-zinc-800">
+					<TableRow className="border-zinc-700 hover:bg-zinc-800">
+						<TableHead className="text-zinc-100">ID</TableHead>
+						<TableHead className="text-zinc-100">Full name</TableHead>
+						<TableHead className="text-zinc-100">E-Mail</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{users.map((user) => (
+						<TableRow key={user.id} className="border-zinc-700 hover:bg-zinc-800 hover:text-zinc-100">
+							<TableCell>{user.id}</TableCell>
+							<TableCell>{user.name}</TableCell>
+							<TableCell>{user.email}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</div>
+	);
+}
